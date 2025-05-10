fc30096ad0adeee4662b902d9b9dcb2ebb5fe014
CodeGeass9527 <lixuanxin9527@gmail.com>
2025-05-11 00:50:29 +0900

    test

--- assets/javascripts/discourse/initializers/badge-avatar-frame-setup.js
+++ assets/javascripts/discourse/initializers/badge-avatar-frame-setup.js
@@ -1,6 +1,8 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 import { ajax } from "discourse/lib/ajax";
 import { popupAjaxError } from "discourse/lib/ajax-error";
+import { alias } from "@ember/object/computed";
+import I18n from "I18n";
 
 export default {
   name: "badge-avatar-frame-setup",
@@ -8,33 +10,36 @@ export default {
     withPluginApi("0.8.31", (api) => {
       // 扩展徽章编辑界面，添加头像框设置
       api.modifyClass("controller:admin-badges-show", {
-        avatarFrameEnabled: Ember.computed.alias("model.avatar_frame_enabled"),
-        avatarFrameUrl: Ember.computed.alias("model.avatar_frame_url"),
+        avatarFrameEnabled: alias("model.avatar_frame_enabled"),
+        avatarFrameUrl: alias("model.avatar_frame_url"),
 
         actions: {
-          saveAvatarFrameSettings() {
-            const badge = this.get("model");
-
-            return ajax(`/admin/badges/${badge.id}/avatar_frame`, {
-              type: "PUT",
-              data: {
-                avatar_frame_enabled: this.avatarFrameEnabled,
-                avatar_frame_url: this.avatarFrameUrl,
-              },
-            })
+          save() {
+            // 调用原有的保存方法
+            this._super(...arguments)
               .then(() => {
-                this.set("savingStatus", I18n.t("saved"));
-                this.set("model.avatar_frame_enabled", this.avatarFrameEnabled);
-                this.set("model.avatar_frame_url", this.avatarFrameUrl);
+                // 保存头像框设置
+                return ajax(
+                  `/admin/badges/${this.get("model.id")}/avatar_frame`,
+                  {
+                    type: "PUT",
+                    data: {
+                      avatar_frame_enabled: this.avatarFrameEnabled,
+                      avatar_frame_url: this.avatarFrameUrl,
+                    },
+                  }
+                );
               })
               .catch(popupAjaxError);
           },
 
           uploadAvatarFrame() {
-            const $fileInput = $("<input type='file' accept='image/*'>");
+            const fileInput = document.createElement("input");
+            fileInput.type = "file";
+            fileInput.accept = "image/*";
 
-            $fileInput.on("change", () => {
-              const file = $fileInput[0].files[0];
+            fileInput.addEventListener("change", () => {
+              const file = fileInput.files[0];
 
               if (file) {
                 const formData = new FormData();
@@ -56,44 +61,44 @@ export default {
               }
             });
 
-            $fileInput.click();
+            fileInput.click();
           },
         },
       });
 
-      // 装饰用户头像，添加头像框
-      api.decorateWidget("user-avatar::after", (helper) => {
-        const attrs = helper.attrs;
+      // // 装饰用户头像，添加头像框
+      // api.decorateWidget("user-avatar::after", (helper) => {
+      //   const attrs = helper.attrs;
 
-        if (attrs.user && attrs.user.avatar_frame_url) {
-          return helper.h("div.badge-avatar-frame", {
-            attributes: {
-              style: `background-image: url('${attrs.user.avatar_frame_url}')`,
-            },
-          });
-        }
-      });
+      //   if (attrs.user && attrs.user.avatar_frame_url) {
+      //     return helper.h("div.badge-avatar-frame", {
+      //       attributes: {
+      //         style: `background-image: url('${attrs.user.avatar_frame_url}')`,
+      //       },
+      //     });
+      //   }
+      // });
 
-      // 装饰用户卡片头像
-      api.reopenWidget("user-card-avatar", {
-        buildClasses() {
-          return `${this._super(attrs)} user-card-avatar`;
-        },
+      // // 装饰用户卡片头像
+      // api.reopenWidget("user-card-avatar", {
+      //   buildClasses(attrs) {
+      //     return `${this._super(attrs)} user-card-avatar`;
+      //   },
 
-        html(attrs) {
-          const user = attrs.user;
+      //   html(attrs) {
+      //     const user = attrs.user;
 
-          const result = this._super(attrs);
-          return [
-            ...result,
-            this.h("div.badge-avatar-frame", {
-              attributes: {
-                style: `background-image: url('${user.avatar_frame_url}')`,
-              },
-            }),
-          ];
-        },
-      });
+      //     const result = this._super(attrs);
+      //     return [
+      //       ...result,
+      //       this.h("div.badge-avatar-frame", {
+      //         attributes: {
+      //           style: `background-image: url('${user.avatar_frame_url}')`,
+      //         },
+      //       }),
+      //     ];
+      //   },
+      // });
 
       // 装饰帖子头像
       api.decorateWidget("post-avatar::after", (helper) => {
